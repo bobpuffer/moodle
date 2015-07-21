f473a44552c2096efed830dd325b2470b3c2416d
Bob Puffer <puffro01@luther.edu>
2015-07-21 09:47:41 -0500

    chmod

--- blocks/morsle/morsle.php
+++ blocks/morsle/morsle.php
@@ -15,8 +15,8 @@ class morsle {
 //        var_dump($key);
 //        die;
         $this->client = new Google_Client();
-	$this->client_id = '1016393342084-dqql4goj9s0l402sbf4dtnoq2tsk0hp8.apps.googleusercontent.com';
-        $this->service_account_name = '1016393342084-dqql4goj9s0l402sbf4dtnoq2tsk0hp8@developer.gserviceaccount.com';
+	$this->client_id = '';
+        $this->service_account_name = '';
         $this->client->addScope("https://www.googleapis.com/auth/drive");
         $this->client->addScope("https://www.googleapis.com/auth/drive.file");
         $this->client->addScope('https://spreadsheets.google.com/feeds'
--- repository/morsle/lib.php
+++ repository/morsle/lib.php
@@ -84,8 +84,8 @@ class repository_morsle extends repository {
 //        var_dump($key);
 //        die;
         $this->client = new Google_Client();
-	$this->client_id = '1016393342084-dqql4goj9s0l402sbf4dtnoq2tsk0hp8.apps.googleusercontent.com';
-        $this->service_account_name = '1016393342084-dqql4goj9s0l402sbf4dtnoq2tsk0hp8@developer.gserviceaccount.com';
+	$this->client_id = '';
+        $this->service_account_name = '';
         $this->client->setScopes(array($this->cal_auth, 
             $this->drive_auth, 
             $this->file_auth, 
